ccb29aac698be67013b38ac7ce346b2a08383a60
iffat-edobo <bnigar@edobo.co>
2024-09-17 23:47:39 +0530

    small changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 mongoose
   .connect("mongodb://localhost:27017/mydatabase")
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Could not connect to MongoDB", err));
+  .catch((err) => console.error("Could not connect to MongoDB....", err));
 
 // Use cart routes
 app.use("/cart", cartRoutes);
